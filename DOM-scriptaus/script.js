6328c7d3568400483c42902ee6b4d4e2115e77a1
sannariipinen <sanna.riipinen@student.laurea.fi>
2023-10-08 22:38:43 +0300

    tyyli

--- DOM-scriptaus/script.js
+++ DOM-scriptaus/script.js
@@ -1,3 +1,17 @@
-<script>
+const inputBox = document.getElementById("teksti");
+const listContainer = document.getElementById("tehtävät");
 
-<script>
+function addTask(){
+    if(inputBox.value === ''){
+        alert("Kirjoita jotain!")
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span)
+    }
+    inputBox.value ="";
+}
