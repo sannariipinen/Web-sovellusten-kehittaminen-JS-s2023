0f08e45aeec5299a5cd0e54a6a758643b202e2ce
sannariipinen <sanna.riipinen@student.laurea.fi>
2023-10-12 18:37:37 +0300

    viimeistelyjä

--- DOM-scriptaus/script.js
+++ DOM-scriptaus/script.js
@@ -1,9 +1,9 @@
-const inputBox = document.getElementById("teksti");
-const listContainer = document.getElementById("tehtävät");
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
 
 function addTask(){
     if(inputBox.value === ''){
-        alert("Kirjoita jotain!")
+        alert("Kirjoita jotain!");
     }
     else{
         let li = document.createElement("li");
@@ -11,27 +11,27 @@ function addTask(){
         listContainer.appendChild(li);
         let span = document.createElement("span");
         span.innerHTML = "\u00d7";
-        li.appendChild(span)
+        li.appendChild(span);
     }
-    inputBox.value ="";
-    saveData()
+    inputBox.value = "";
+    saveData();
 }
-
-listContainer.addEventListener("click", function(e)){
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+listContainer.addEventListener("click", function(e){
     if(e.target.tagName === "LI"){
-        e.target.classList.toggle("tehty");
-        saveData()
+        e.target.classList.toggle("checked");
+        saveData();
     }
     else if(e.target.tagName === "SPAN"){
         e.target.parentElement.remove();
-        saveData()
+        saveData();
     }
 }, false);
 
 function saveData(){
     localStorage.setItem("data", listContainer.innerHTML);
 }
-function showTask(){
-    listContainer.innerHTML = localStorage.getItem("data");
-}
+
 showTask();
