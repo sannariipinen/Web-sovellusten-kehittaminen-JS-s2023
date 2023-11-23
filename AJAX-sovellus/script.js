0cd0336c2a7d6f99619fd463a830ec3cb156d3c3
Nelli Nieminen <nellin22@gmail.com>
2023-11-23 13:26:01 +0200

    katotaan

--- AJAX-sovellus/script.js
+++ AJAX-sovellus/script.js
@@ -1,92 +1,100 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var url = "https://www.finnkino.fi/xml/TheatreAreas/";
-    var xmlhttp = new XMLHttpRequest();
-    var data;
+function populateTheaters() {
+    fetch('https://www.finnkino.fi/xml/TheatreAreas/')
+        .then(response => response.text())
+        .then(str => new DOMParser().parseFromString(str, "text/xml"))
+        .then(data => {
+            const theaters = Array.from(data.querySelectorAll('TheatreArea'));
+            theaters.sort((a, b) => a.querySelector('Name').textContent.localeCompare(b.querySelector('Name').textContent));
 
-    xmlhttp.open("GET", url, true);
-    xmlhttp.send();
-
-    xmlhttp.onreadystatechange = function () {
-        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-            data = xmlhttp.responseXML;
-            var theaterDropdown = document.getElementById("theater-dropdown");
-
-            theaterDropdown.addEventListener("change", function () {
-                var selectedTheaterId = theaterDropdown.value;
-                var movies = getMoviesByTheater(data, selectedTheaterId);
-                displayMovies(movies);
+            const dropdown = document.getElementById('theater-dropdown');
+            dropdown.innerHTML = '<option>Valitse teatteri</option>';
+            theaters.forEach(theater => {
+                const option = document.createElement('option');
+                option.value = theater.querySelector('ID').textContent;
+                option.textContent = theater.querySelector('Name').textContent;
+                dropdown.appendChild(option);
             });
+        })
+        .catch(error => {
+            console.error('Virhe haettaessa teattereita:', error);
+            document.getElementById('theater-dropdown').innerHTML = '<option>Teattereiden lataus epäonnistui</option>';
+        });
+}
+function fetchMovies(theaterId) {
+    const today = new Date();
+    const formattedDate = `${today.getFullYear()}-${(today.getMonth() + 1).toString().padStart(2, '0')}-${today.getDate().toString().padStart(2, '0')}`;
+    const url = `https://www.finnkino.fi/xml/Schedule/?area=${theaterId}&dt=${formattedDate}`;
+
+    fetch(url)
+        .then(response => response.text())
+        .then(str => new DOMParser().parseFromString(str, "text/xml"))
+        .then(data => {
+            const shows = data.querySelectorAll('Show');
+            const movies = [];
+            shows.forEach(show => {
+                const title = show.querySelector('Title').textContent;
+                const originalTitle = show.querySelector('OriginalTitle').textContent;
+                const showTime = show.querySelector('dttmShowStart').textContent;
+                movies.push({ title, originalTitle, showTime });
+            });
+
+            const moviesContainer = document.getElementById('movies-container');
+            moviesContainer.innerHTML = '';
+
+            movies.forEach(movie => {
+                fetchMovieInfo(movie.originalTitle, moviesContainer, movie.showTime);
+            });
+        })
+        .catch(error => console.error('Virhe haettaessa elokuvia:', error));
+}
+function displayMovieInfo(movieInfo, container, showTime) {
+    const showTimeFormatted = new Date(showTime).toLocaleString('fi-FI', {
+        day: '2-digit',
+        month: '2-digit',
+        year: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+        hour12: false
+    });
+    const movieElement = document.createElement('div');
+    movieElement.className = 'movie';
+    movieElement.innerHTML = `
+        <img src="${movieInfo.Poster}" alt="${movieInfo.Title} juliste">
+        <h3>${movieInfo.Title}</h3>
+        <p>Näytösaika: ${showTimeFormatted}</p>
+        <p>Genre: ${movieInfo.Genre}</p>
+        <p>Ohjaaja: ${movieInfo.Director}</p>
+        <p>Näyttelijät: ${movieInfo.Actors}</p>
+        <p>Kesto: ${movieInfo.Runtime}</p>
+        <p>Ikäraja: ${movieInfo.Rated}</p>
+        <details>
+            <summary>Juoni (Lue klikkaamalla)</summary>
+            <p>${movieInfo.Plot}</p>
+        </details>
+        <button class="add-to-wishlist-button" data-added="false">Lisää toivelistaan</button>
+    `;
+    container.appendChild(movieElement);
+
+    const wishlistButton = movieElement.querySelector('.add-to-wishlist-button');
+    wishlistButton.addEventListener('click', handleWishlistButtonClick);
+}
+function filterMovies() {
+    const searchValue = document.getElementById('search-input').value.toLowerCase();
+    const movies = document.querySelectorAll('.movie');
+    movies.forEach(movie => {
+        const title = movie.querySelector('h3').textContent.toLowerCase();
+        if (title.includes(searchValue)) {
+            movie.style.display = '';
+        } else {
+            movie.style.display = 'none';
         }
-    };
-
-    function getMoviesByTheater(data, theaterId) {
-        var theaterElements = data.querySelectorAll('TheatreArea');
-        var movies = [];
-
-        for (var i = 0; i < theaterElements.length; i++) {
-            var id = theaterElements[i].getElementsByTagName("ID")[0].textContent;
-
-            if (id === theaterId) {
-                var titleElements = theaterElements[i].getElementsByTagName("Title");
-                for (var j = 0; j < titleElements.length; j++) {
-                    var title = titleElements[j].textContent;
-                    movies.push(title);
-                }
-                break; // Stop searching once the theater is found
-            }
-        }
-
-        return movies;
-    }
-
-    function displayMovies(movies) {
-        var movieList = document.getElementById("movie-list");
-        movieList.innerHTML = ""; // Clear previous content
-
-        for (var i = 0; i < movies.length; i++) {
-            var listItem = document.createElement("li");
-            listItem.textContent = movies[i];
-            movieList.appendChild(listItem);
-        }
+    });
+}
+document.addEventListener('DOMContentLoaded', populateTheaters);
+document.getElementById('theater-dropdown').addEventListener('change', function() {
+    const theaterId = this.value;
+    if (theaterId) {
+        fetchMovies(theaterId);
     }
 });
-document.addEventListener("DOMContentLoaded", function () {
-    var scheduleUrl = "https://www.finnkino.fi/xml/Schedule/?area=";
-    var xmlhttp = new XMLHttpRequest();
-    var data;
-
-    xmlhttp.open("GET", "https://www.finnkino.fi/xml/TheatreAreas/", true);
-    xmlhttp.send();
-
-    xmlhttp.onreadystatechange = function () {
-        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-            data = xmlhttp.responseXML;
-            var theaterDropdown = document.getElementById("theater-dropdown");
-
-            theaterDropdown.addEventListener("change", function () {
-                var selectedTheaterId = theaterDropdown.value;
-                var scheduleApiUrl = scheduleUrl + selectedTheaterId;
-                
-                fetchScheduleData(scheduleApiUrl);
-            });
-        }
-    };
-
-    function fetchScheduleData(apiUrl) {
-        var scheduleXmlhttp = new XMLHttpRequest();
-        scheduleXmlhttp.open("GET", apiUrl, true);
-        scheduleXmlhttp.send();
-
-        scheduleXmlhttp.onreadystatechange = function () {
-            if (scheduleXmlhttp.readyState == 4 && scheduleXmlhttp.status == 200) {
-                var scheduleData = scheduleXmlhttp.responseXML;
-                displaySchedule(scheduleData);
-            }
-        };
-    }
-
-    function displaySchedule(scheduleData) {
-        // Customize this function to display the schedule data as needed
-        console.log(scheduleData);
-    }
-});
+document.getElementById('search-input').addEventListener('input', filterMovies);
