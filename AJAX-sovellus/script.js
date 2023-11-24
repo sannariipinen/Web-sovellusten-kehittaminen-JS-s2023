a7d695450c10a5a2751cfd57f0798eb1f5f051ab
sannariipinen <sanna.riipinen@student.laurea.fi>
2023-11-24 12:37:28 +0200

    jee

--- AJAX-sovellus/script.js
+++ AJAX-sovellus/script.js
@@ -1,6 +1,36 @@
+function haeTeatterit() {
+    fetch('https://www.finnkino.fi/xml/TheatreAreas/')
+        .then(response => response.text())
+        .then(str => new DOMParser().parseFromString(str, "text/xml"))
+        .then(data => {
+            const teatterit = data.querySelectorAll('TheatreArea');
+
+            for (let i = 0; i < teatterit.length; i++) {
+                let teatteri = teatterit[i];
+                let teatteriNimi = teatteri.getElementsByTagName('Name')[0].innerHTML;
+                let teatteriId = teatteri.getElementsByTagName('ID')[0].innerHTML;
+
+                let html = `
+                    <option value="${teatteriId}">${teatteriNimi}</option>
+                `;
+                
+                document.getElementById('theater-dropdown').innerHTML += html;
+            }
+
+            // Voit kutsua tässä haeElokuvat()-funktiota tai muuta tarvittavaa
+        })
+        .catch(error => {
+            console.error('Virhe haettaessa teattereita:', error);
+            document.getElementById('theater-dropdown').innerHTML = '<option>Teattereiden lataus epäonnistui</option>';
+        });
+}
+
+// Kutsu haeTeatterit()-funktiota tarpeen mukaan
+haeTeatterit();
+
 function haeElokuvat() {
     fetch('https://www.finnkino.fi/xml/Schedule/?area=1031&dt=24.11.2023')
-    
+
         .then(response => response.text())
         .then(str => new DOMParser().parseFromString(str, "text/xml"))
         .then(data => {
