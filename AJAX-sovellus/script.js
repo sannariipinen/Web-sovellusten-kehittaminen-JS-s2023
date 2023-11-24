af7fa425102bb1c00fcb63c4f2351ea337aa648b
”zumratzumureti” <zumrat.zumureti@outlook.com>
2023-11-24 11:55:54 +0200

    c

--- AJAX-sovellus/script.js
+++ AJAX-sovellus/script.js
@@ -1,32 +1,23 @@
-// Teatterivalikon näyttäminen
-function populateTheaters() {
-    fetch('https://www.finnkino.fi/xml/TheatreAreas/')
+function haeElokuvat() {
+    fetch('https://www.finnkino.fi/xml/Schedule/?area=1031&dt=24.11.2023')
+
         .then(response => response.text())
         .then(str => new DOMParser().parseFromString(str, "text/xml"))
         .then(data => {
-            const theaters = Array.from(data.querySelectorAll('TheatreArea'));
-            theaters.sort((a, b) => a.querySelector('Name').textContent.localeCompare(b.querySelector('Name').textContent));
+            const elokuvat = (data.querySelectorAll('Show'));
+            
+        console.log (elokuvat)
+            for (let i= 0; i<elokuvat.length; i++){
+                let show= elokuvat[i].innerHTML
+            console.log (show)
+            let Title= show.getElementsByTagName('Title')
 
-            const dropdown = document.getElementById('theater-dropdown');
-            dropdown.innerHTML = '<option>Valitse teatteri</option>';
-            theaters.forEach(theater => {
-                const option = document.createElement('option');
-                option.value = theater.querySelector('ID').textContent;
-                option.textContent = theater.querySelector('Name').textContent;
-                dropdown.appendChild(option);
-            });
+            console.log(Title)
+            }
         })
         .catch(error => {
             console.error('Virhe haettaessa teattereita:', error);
             document.getElementById('theater-dropdown').innerHTML = '<option>Teattereiden lataus epäonnistui</option>';
         });
     }
-//Teatterivalikon alustus
-document.addEventListener('DOMContentLoaded', populateTheaters);
-//Teatterivalikon muutosten kuuntelija
-document.getElementById('theater-dropdown').addEventListener('change', function() {
-    const theaterId = this.value;
-    if (theaterId) {
-        fetchMovies(theaterId);
-    }
-});
+
