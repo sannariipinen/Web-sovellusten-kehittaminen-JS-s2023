6b94b4d5ffdb6757735eff75f80164ec3aadcda8
Nelli Nieminen <nellin22@gmail.com>
2023-11-24 10:46:45 +0200

    toimiiko?

--- AJAX-sovellus/script.js
+++ AJAX-sovellus/script.js
@@ -1,8 +1,40 @@
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-       document.getElementById("theater-dropdown").innerHTML = xhttp.responseText;
+document.addEventListener("DOMContentLoaded", function () {
+    var url = "https://www.finnkino.fi/xml/TheatreAreas/";
+    var xmlhttp = new XMLHttpRequest();
+    var data;
+
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            data = xmlhttp.responseXML;
+            var theaterDropdown = document.getElementById("theater-dropdown");
+
+            theaterDropdown.addEventListener("change", function () {
+                var selectedTheaterId = theaterDropdown.value;
+                var movies = getMoviesByTheater(data, selectedTheaterId);
+                displayMovies(movies);
+            });
+        }
+    };
+
+    function getMoviesByTheater(data, theaterId) {
+        var theaterElements = data.querySelectorAll('TheatreArea');
+        var movies = [];
+
+        for (var i = 0; i < theaterElements.length; i++) {
+            var id = theaterElements[i].getElementsByTagName("ID")[0].textContent;
+
+            if (id === theaterId) {
+                var titleElements = theaterElements[i].getElementsByTagName("Title");
+                for (var j = 0; j < titleElements.length; j++) {
+                    var title = titleElements[j].textContent;
+                    movies.push(title);
+                }
+                break; // Stop searching once the theater is found
+            }
+        }
+
+        return movies;
     }
-};
-xhttp.open("GET", "https://www.finnkino.fi/xml/TheatreAreas/", true);
-xhttp.send();
