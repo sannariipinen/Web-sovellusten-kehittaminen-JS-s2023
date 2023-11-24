b268c736307c8bfa8b8fc9d59c57353e0e009212
”zumratzumureti” <zumrat.zumureti@outlook.com>
2023-11-24 13:07:56 +0200

    p

--- AJAX-sovellus/script.js
+++ AJAX-sovellus/script.js
@@ -14,7 +14,7 @@ function haeTeatterit() {
                     <option value="${teatteriId}">${teatteriNimi}</option>
                 `;
                 
-                document.getElementById('theater-dropdown').innerHTML += html;
+                document.querySelector('HaeTeatterit').innerHTML += html
             }
 
             // Voit kutsua tässä haeElokuvat()-funktiota tai muuta tarvittavaa
