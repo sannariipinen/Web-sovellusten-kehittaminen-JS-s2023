0afbde161e7282ef9b504063d1c6939f6d723836
Nelli Nieminen <nellin22@gmail.com>
2023-11-24 10:51:27 +0200

    hmm

--- AJAX-sovellus/script.js
+++ AJAX-sovellus/script.js
@@ -1,40 +1,32 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var url = "https://www.finnkino.fi/xml/TheatreAreas/";
-    var xmlhttp = new XMLHttpRequest();
-    var data;
+// Teatterivalikon näyttäminen
+function populateTheaters() {
+    fetch('https://www.finnkino.fi/xml/TheatreAreas/')
+        .then(response => response.text())
+        .then(str => new DOMParser().parseFromString(str, "text/xml"))
+        .then(data => {
+            const theaters = Array.from(data.querySelectorAll('TheatreArea'));
+            theaters.sort((a, b) => a.querySelector('Name').textContent.localeCompare(b.querySelector('Name').textContent));
 
-    xmlhttp.open("GET", url, true);
-    xmlhttp.send();
-
-    xmlhttp.onreadystatechange = function () {
-        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-            data = xmlhttp.responseXML;
-            var theaterDropdown = document.getElementById("theater-dropdown");
-
-            theaterDropdown.addEventListener("change", function () {
-                var selectedTheaterId = theaterDropdown.value;
-                var movies = getMoviesByTheater(data, selectedTheaterId);
-                displayMovies(movies);
+            const dropdown = document.getElementById('theater-dropdown');
+            dropdown.innerHTML = '<option>Valitse teatteri</option>';
+            theaters.forEach(theater => {
+                const option = document.createElement('option');
+                option.value = theater.querySelector('ID').textContent;
+                option.textContent = theater.querySelector('Name').textContent;
+                dropdown.appendChild(option);
             });
-        }
-    };
-
-    function getMoviesByTheater(data, theaterId) {
-        var theaterElements = data.querySelectorAll('TheatreArea');
-        var movies = [];
-
-        for (var i = 0; i < theaterElements.length; i++) {
-            var id = theaterElements[i].getElementsByTagName("ID")[0].textContent;
-
-            if (id === theaterId) {
-                var titleElements = theaterElements[i].getElementsByTagName("Title");
-                for (var j = 0; j < titleElements.length; j++) {
-                    var title = titleElements[j].textContent;
-                    movies.push(title);
-                }
-                break; // Stop searching once the theater is found
-            }
-        }
-
-        return movies;
+        })
+        .catch(error => {
+            console.error('Virhe haettaessa teattereita:', error);
+            document.getElementById('theater-dropdown').innerHTML = '<option>Teattereiden lataus epäonnistui</option>';
+        });
+}
+//Teatterivalikon alustus
+document.addEventListener('DOMContentLoaded', populateTheaters);
+//Teatterivalikon muutosten kuuntelija
+document.getElementById('theater-dropdown').addEventListener('change', function() {
+    const theaterId = this.value;
+    if (theaterId) {
+        fetchMovies(theaterId);
     }
+});
