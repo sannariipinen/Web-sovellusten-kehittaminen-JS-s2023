d2684fab3efafa74a35de809a933781e169d59c5
”zumratzumureti” <zumrat.zumureti@outlook.com>
2023-11-30 11:09:35 +0200

    c

--- AJAX-sovellus/script.js
+++ AJAX-sovellus/script.js
@@ -119,28 +119,28 @@ function haeElokuvat(selectedTheater, selectedDate) {
             let formattedDateTime = `${showTime.getDate()}.${showTime.getMonth() + 1}. klo ${showTime.getHours()}.${showTime.getMinutes()}`;
             let Title= show.getElementsByTagName('Title')[0].innerHTML;
             let Genres= show.getElementsByTagName('Genres') [0].innerHTML;
-            let EventSmallImagePortrait =show.getElementsByTagName ('EventSmallImagePortrait') [0].innerHTML;
+            let EventMediumImagePortrait= show.getElementsByTagName ('EventMediumImagePortrait') [0].innerHTML;
             let Name= show.getElementsByTagName ('Name') [0].innerHTML;
             let RatingImageUrl = show.getElementsByTagName ('RatingImageUrl') [0].innerHTML;
 
             console.log(Title)
             console.log(Genres)
             console.log(formattedDateTime)
-            console.log(EventSmallImagePortrait)
+            console.log(EventMediumImagePortrait)
             console.log(Name)
             console.log(RatingImageUrl)
 
             let elokuvaContainer = document.createElement('div');
             elokuvaContainer.classList.add('elokuva-container');
             let html= `
-            <img src= "${EventSmallImagePortrait}" alt="${Title}">
+            <img src= "${EventMediumImagePortrait}" alt="${Title}">
             <div class="elokuva-tiedot">
             <h1>${Title}</h1>
             <h2>${Genres}</h2>
             <h2>${formattedDateTime}</h2>
             <h2>${Name}</h2>
+            <img src= "${RatingImageUrl}" alt="${Genres}">   
             <button class="wishlist-button" data-title="${Title}">&#10084; Lisää toivelistalle </button>
-            <img src= "${RatingImageUrl}" alt="${Genres}">    
             </div>`
             console.log(html)
             ; 
