b5f65c5c9f331e602ea235ac8ae3750383c4a500
sannariipinen <sanna.riipinen@student.laurea.fi>
2023-12-01 10:11:33 +0200

    h

--- jQuery/script.js
+++ jQuery/script.js
@@ -1,55 +1,30 @@
+//Ladataan sivu, kun DOM on valmis
 document.addEventListener('DOMContentLoaded', function () {
-  const theaterDropdown = document.getElementById('theaterDropdown');
-  const dateDropdown = document.getElementById('dateDropdown');
-
-  // Event listener for theater dropdown changes
-  theaterDropdown.addEventListener('change', function () {
-      const selectedTheater = theaterDropdown.value;
-      updateMovies(selectedTheater, null);
-      loadDates(selectedTheater);
-  });
-
-  // Load dates for the default theater
-  loadDates(theaterDropdown.value);
-  loadWishlistFromStorage();
-});
-
-function loadDates(selectedTheater) {
-  fetch(`https://www.finnkino.fi/xml/ScheduleDates/?area=${selectedTheater}`)
+  //Haetaan elokuvien näytösajat Finnkinon XML-linkistä
+  fetch(`https://www.finnkino.fi/xml/ScheduleDates/`)
       .then(response => response.text())
       .then(str => new DOMParser().parseFromString(str, "text/xml"))
       .then(data => {
-          const dateDropdown = document.getElementById('dateDropdown');
-          const today = new Date();
-          const todayISO = today.toISOString().split('T')[0];
-
-          // Clear existing options
-          dateDropdown.innerHTML = '<option>Valitse päivämäärä</option>';
-
-          // Get all dates from the XML
-          const dates = Array.from(data.querySelectorAll('dateTime'));
-
-          // Filter dates to keep only the ones for the current day
-          const currentDayDates = dates.filter(date => {
-              return date.innerHTML.split('T')[0] === todayISO;
-          });
-
-          // Populate the date dropdown with available dates for the current day
-          currentDayDates.forEach(date => {
-              const option = document.createElement('option');
-              option.value = date.innerHTML;
-              option.textContent = new Date(date.innerHTML).toLocaleDateString('en-FI', { weekday: 'long', year: 'numeric', month: 'numeric', day: 'numeric' });
-              dateDropdown.appendChild(option);
-          });
-
-          // Set the selected date to today
-          dateDropdown.value = todayISO;
+  
+        const today = new Date();
+        const todayISO = today.toISOString().split('T')[0];
+//lisätään päivämäärä valintaan nykyinen päivä
+        const option = document.createElement('option');
+        option.value = todayISO;
+        option.textContent = today.toLocaleDateString('en-FI', { weekday: 'long', year: 'numeric', month: 'numeric', day: 'numeric' });
+        dateDropdown.appendChild(option);
+        document.getElementById('dateDropdown').value = todayISO;
+//päivitetään elokuvat valitulla teatterilla ja päivämäärällä
+        updateMovies(document.getElementById('theaterDropdown').value, todayISO);
+        
       })
+       
       .catch(error => console.error('Error fetching schedule dates:', error));
-}
+});
 
 loadWishlistFromStorage();
 
+//ladataan toivelistan tiedot selaimen muistista
 function loadWishlistFromStorage() {
   const wishlist = JSON.parse(localStorage.getItem('wishlist')) || {};
   const wishlistButtons = document.querySelectorAll('.wishlist-button');
@@ -59,7 +34,7 @@ function loadWishlistFromStorage() {
     updateButtonColor(button, wishlist[title]);
   });
 }
-
+//päivitetään toivelistaa
 function updateWishlistUI() {
     const wishlistContainer = document.getElementById('wishlist-list');
     wishlistContainer.innerHTML = '';
@@ -69,7 +44,7 @@ function updateWishlistUI() {
     for (const title in wishlist) {
         if (wishlist.hasOwnProperty(title)) {
             const listItem = document.createElement('li');
-            listItem.textContent = title;
+            listItem.textContent = `• ${title}`;
             wishlistContainer.appendChild(listItem);
         }
     }
@@ -77,39 +52,27 @@ function updateWishlistUI() {
 
 updateWishlistUI();
 
-// Modify updateMovies function to default to tomorrow's date
+// Muokataan funktiota, että ohjelma hakee tietyn teatterin elokuvat
 function updateMovies(selectedTheater, selectedDate) {
   console.log("Update Movies - Theater: ", selectedTheater);
   console.log("Update Movies - Date:", selectedDate);
 
-
-  if (!selectedDate) {
-      const tomorrow = new Date();
-      tomorrow.setDate(tomorrow.getDate() + 1);
-      selectedDate = tomorrow.toISOString().split('T')[0];
-      document.getElementById('dateDropdown').value = selectedDate;
-      
-  }
-
-
+    
   console.log("After Default - Update Movies - Date:", selectedDate);
   haeElokuvat(selectedTheater, selectedDate);
 }
-document.addEventListener('DOMContentLoaded', function () {
+// Lisätään tapahtumakuuntelija toivelistan painikkeelle
   const wishlist = JSON.parse(localStorage.getItem('wishlist')) || {};
   const movieScheduleDiv = document.getElementById('laatikko');
 
-  // Event listener for all buttons inside the movie container
   movieScheduleDiv.addEventListener('click', function (event) {
       const target = event.target;
-
-      // Check if the clicked element is a button with the 'wishlist-button' class
       if (target.classList.contains('wishlist-button')) {
           toggleWishlist(target);
       }
   });
-});
 
+//Elokuva lisätään toivelistaan nappia painamalla
 function toggleWishlist(button) {
   const title = button.getAttribute('data-title');
   let wishlist = JSON.parse(localStorage.getItem('wishlist')) || {};
@@ -119,20 +82,21 @@ function toggleWishlist(button) {
   } else {
       wishlist[title] = true;
   }
-
+// Tallennetaan toivelistan tiedot selaimen muistiin
   localStorage.setItem('wishlist', JSON.stringify(wishlist));
 
-  // Update the variable after setting it in local storage
+  // Päivitetään napin väriä ja toivelistaa
   wishlist = JSON.parse(localStorage.getItem('wishlist')) || {};
-
   console.log('Updated wishlist:', wishlist);
   updateButtonColor(button, wishlist[title]);
-  updateWishlistUI(); // Päivitä wishlistin näyttö
+  updateWishlistUI();
 };
 
 function updateButtonColor(button, title) {
   console.log('Updating button color for:', title);
   const isInWishlist = title === true;
+
+//Lisätään tai poistetaan elokuva toivelistalta
   if (isInWishlist) {
       button.classList.add('wishlist');
   } else {
@@ -140,18 +104,14 @@ function updateButtonColor(button, title) {
   }
 }
 
-const wishlistButtons = document.querySelectorAll('.wishlist-button');
-
-
-document.addEventListener('DOMContentLoaded', function () {
   const wishlistButtons = document.querySelectorAll('.wishlist-button');
 
   wishlistButtons.forEach(button => {
       const title = button.getAttribute('data-title');
       updateButtonColor(button);
     });
-  });      
-
+      
+//Haetaan elokuvat Finnkinon XML-linkistä
 function haeElokuvat(selectedTheater, selectedDate) {
     console.log("Hae Elokuvat - Theater: ", selectedTheater);
     console.log("Hae Elokuvat - Date:", selectedDate);
@@ -177,27 +137,28 @@ function haeElokuvat(selectedTheater, selectedDate) {
             let formattedDateTime = `${showTime.getDate()}.${showTime.getMonth() + 1}. klo ${showTime.getHours()}.${showTime.getMinutes()}`;
             let Title= show.getElementsByTagName('Title')[0].innerHTML;
             let Genres= show.getElementsByTagName('Genres') [0].innerHTML;
-            let EventSmallImagePortrait =show.getElementsByTagName ('EventSmallImagePortrait') [0].innerHTML;
+            let EventMediumImagePortrait =show.getElementsByTagName ('EventMediumImagePortrait') [0].innerHTML;
             let Name= show.getElementsByTagName ('Name') [0].innerHTML;
             let RatingImageUrl = show.getElementsByTagName ('RatingImageUrl') [0].innerHTML;
 
             console.log(Title)
             console.log(Genres)
             console.log(formattedDateTime)
-            console.log(EventSmallImagePortrait)
+            console.log(EventMediumImagePortrait)
             console.log(Name)
             console.log(RatingImageUrl)
-
+ // Luodaan div-elementti, joka sisältää elokuvan tiedot 
             let elokuvaContainer = document.createElement('div');
             elokuvaContainer.classList.add('elokuva-container');
             let html= `
-            <img src= "${EventSmallImagePortrait}" alt="${Title}">
+            <img src= "${EventMediumImagePortrait}" alt="${Title}">
             <div class="elokuva-tiedot">
             <h1>${Title}</h1>
             <h2>${Genres}</h2>
             <h2>${formattedDateTime}</h2>
             <h2>${Name}</h2>
             <img src= "${RatingImageUrl}" alt="${Genres}">
+            <br>
             <button class="wishlist-button" data-title="${Title}">&#10084; Lisää toivelistalle </button>
         
             </div>`
